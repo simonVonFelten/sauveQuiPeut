db2d2d16a87b79527702ca4d61de0587e8d39471
SimonVonFelten <simon.von_felten@edu.univ-fcomte.fr>
2021-12-03 05:31:11 +0100

    baguette

--- js/targets.mjs
+++ js/targets.mjs
@@ -2,16 +2,23 @@ let maxWidth = 3000;
 let minWidth = -3000;
 let sea = document.getElementById("sea");
 let srcImg = "./images/target.png";
+let srcImgBaguette = "./images/baguette.png";
 
 let widthWindow = window.innerWidth;
 
 class Target {
 
-    constructor(x){
+    constructor(x,baquette=false){
         this.x =x;
+        this.baguette = baquette;
         this.element = document.createElement('img');
-        this.element.src=srcImg;
-        this.element.alt="naufragés";
+        if(baquette){
+            this.element.src=srcImgBaguette;
+            this.element.alt="naufragésBaguette";
+        }else {
+            this.element.src=srcImg;
+            this.element.alt="naufragés";
+        }
         this.element.style.left=this.x+"px";
         this.element.style.position="absolute";
         this.element.classList.add("target");
@@ -19,6 +26,10 @@ class Target {
 
     display(){
         sea.appendChild(this.element);
+    }
+
+    isBaguette(){
+        return this.baguette;
     }
 
     move(x){
@@ -53,6 +64,9 @@ export function initTargets(nbTargets){
 
     for(let i=0;i<nbTargets;i++){
         let t = new Target( Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth));
+        if(Math.floor(Math.random()*20)==1){
+            t = new Target( Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth),true);
+        }
         t.display()
         targets.push(t);
         targetsVerify.push(t);
@@ -83,7 +97,11 @@ export function isTargetPos (positionFilet,position,isRight){
     let count=0;
     for (let i=0;i<targetsVerify.length;i++){
         if(targetsVerify[i].isCaptured(positionFilet,isRight)){
-            count++;
+            if(targetsVerify[i].isBaguette()){
+                count+=2;
+            }else{
+                count++;
+            }
             let toMove =targetsVerify[i].getElement();
             toMove.classList.add("transitionLeft");
             toMove.style.left=position+"px";
